edb264cec13161f2067fa1ac5f2ad362f1167b96
danielsg <101757213+D4n13lsg@users.noreply.github.com>
2022-10-11 16:08:45 -0500

    Update get_user_name.dart

--- login/lib/data/get_user_name.dart
+++ login/lib/data/get_user_name.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-//se crea 
+//se crea una clase 
 class GetUserName extends StatelessWidget {
   final String documentId;
   GetUserName({required this.documentId});
