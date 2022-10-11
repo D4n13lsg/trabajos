4c45c2d1efc349c5776ba4fbd30d33c48403dea0
danielsg <101757213+D4n13lsg@users.noreply.github.com>
2022-10-11 15:39:49 -0500

    Update card.dart

--- login/lib/card.dart
+++ login/lib/card.dart
@@ -4,7 +4,10 @@ import 'package:login/pages/Login_page.dart';
 class card extends StatelessWidget{
 
   @override
+  //Lo primero que todo desarrollador de Flutter/Dart debe comprender es que en este marco «TODO ES UN WIDGET». 
+  //Los widgets son básicamente componentes de la interfaz de usuario utilizados para crear la interfaz de usuario de la aplicación (valga la redondancia)
   Widget build(BuildContext context) {
+    //Entonces miremos un ejemplo de uso. Hemos utilizado la AppBar, el body es un widget, puede usar el widget que desees. 
     return Scaffold(
         backgroundColor: Color(0xFFFFFFFF),
         appBar: AppBar(
