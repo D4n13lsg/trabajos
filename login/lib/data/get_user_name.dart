428f37842f015a96264a119e5418ceba590e187b
danielsg <101757213+D4n13lsg@users.noreply.github.com>
2022-10-11 16:07:34 -0500

    Update get_user_name.dart

--- login/lib/data/get_user_name.dart
+++ login/lib/data/get_user_name.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//se crea 
 class GetUserName extends StatelessWidget {
   final String documentId;
   GetUserName({required this.documentId});
 
   @override
+  // Los widgets son básicamente componentes de la interfaz de usuario utilizados para crear la interfaz de usuario de la aplicación 
   Widget build(BuildContext context) {
     CollectionReference users = FirebaseFirestore.instance.collection('cuentas');
 
+    //se crea un retorno
     return FutureBuilder<DocumentSnapshot>(
         future: users.doc(documentId).get(),
         builder: ((context, shapshot){
